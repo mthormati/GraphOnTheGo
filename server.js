1ca68b2ed63d3b444eb0199d904cb333e787a376
Matin Hormati <mthormati@gmail.com>
2017-09-30 11:40:55 -0400

    Restart project

--- server.js
+++ server.js
@@ -17,15 +17,16 @@ var corsOptions = {
   credentials: true
 };
 
-app.get('/typeahead', cors(corsOptions), require('./api/typeahead'));
-app.get('/resolver', cors(corsOptions), require('./api/resolver'));
 // The editor interface.
 app.get('/editor', function(req, res) {
   res.sendFile(__dirname + '/editor.html');
 });
 
+// The in-email representation.
+app.post('/api/resolver', cors(corsOptions), require('./api/resolver'));
+
 if (process.env.NODE_ENV === 'production') {
-  app.listen(process.env.PORT || 9145);
+  app.listen(process.env.PORT || 8910);
 } else {
   var pem = require('pem');
   var https = require('https');
@@ -35,6 +36,6 @@ if (process.env.NODE_ENV === 'production') {
     https.createServer({
       key: keys.serviceKey,
       cert: keys.certificate
-    }, app).listen(process.env.PORT || 9145);
+    }, app).listen(process.env.PORT || 8910);
   });
 }
